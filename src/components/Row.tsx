bfff5c8b615802ffa8314f768199da204e168d9d
5maciek <smaciek1@op.pl>
2020-06-09 22:49:55 +0200

    small refactor Row component

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,40 +1,48 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect } from 'react';
+import { TR } from './styled/TR';
+import { TD } from './styled/TD';
 import { v4 as uuidv4 } from 'uuid';
 
 function Row(props: any) {
-  const TR = styled.tr`
-    &:nth-child(even) {
-      background-color: #e0d4bb;
-    }
-  `;
-
-  const TD = styled.td<{ lastColumn: boolean }>`
-    background-color: ${(props) => (props.lastColumn ? '#CAB388;' : '')}
-    position: ${(props) => (props.lastColumn ? 'sticky;' : 'inherit;')}
-    right: ${(props) => (props.lastColumn ? '0;' : 'auto;')}
-    text-align: center;
-    padding: 10px;
-
-  `;
-
   const sumRow = () => {
-    const sumtotal = props.row.reduce((sum: any, val: any) => {
-      if (!isNaN(val)) {
+    const sumtotal = props.row.reduce((sum: any, val: any, index: any) => {
+      if (!isNaN(val) && index < props.countColumns) {
         return sum + val;
       }
       return sum;
     }, 0);
-
     return sumtotal;
   };
 
+  const fillEmpty = () => {
+    if (props.row.length < props.countColumns) {
+      const fillArray: any = [];
+      for (let i = 0; i < props.row.length - props.countColumns; i++) {
+        fillArray.push('111');
+      }
+      return fillArray;
+    }
+  };
+
+  useEffect(() => {
+    if (props.row.length > props.countColumns) {
+      console.log(
+        'Some data rows are not displayed because is mismatch with columns.'
+      );
+    }
+  });
+  const arr: any = fillEmpty();
   return (
     <TR>
-      {props.row.map((item: any) => (
-        <TD key={uuidv4()} lastColumn={false}>
-          {item}
-        </TD>
+      {props.row.map((item: any, index: any) =>
+        index < props.countColumns ? (
+          <TD key={uuidv4()} lastColumn={false}>
+            {item}
+          </TD>
+        ) : null
+      )}
+      {arr.map((item: any) => (
+        <td>item</td>
       ))}
       <TD key={uuidv4()} lastColumn={true}>
         {sumRow()}

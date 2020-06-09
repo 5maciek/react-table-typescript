02d7db7e02410fe3f6c2d3e8dd0d73926a2aeffd
5maciek <smaciek1@op.pl>
2020-06-09 22:51:13 +0200

    small refactor Column component

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const padding = '8px;';
+import { TH } from './styled/TH';
 
 function Column(props: any) {
-  const TH = styled.th<{ widthColumn: number; lastColumn: boolean }>`    
-    width: ${(props) =>
-      props.widthColumn ? props.widthColumn + 'px;' : '100px;'}
-    background-color: ${(props) => (props.lastColumn ? '#CAB388;' : '#D5C4A1;')}
-    position: ${(props) => (props.lastColumn ? 'sticky;' : 'inherit;')}
-    right: ${(props) => (props.lastColumn ? '0;' : 'auto;')}
-    padding: ${padding}    
-    font-weight: bold;
-    cursor: pointer;
-  `;
-
   return (
     <TH
       widthColumn={props.width}

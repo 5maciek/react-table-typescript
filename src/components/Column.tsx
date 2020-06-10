daa17b5cf27071074bf68accca082191fa86ed5a
5maciek <smaciek1@op.pl>
2020-06-10 10:55:42 +0200

    add possibility to display <i> on column title

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TH } from './styled/TH';
+import 'font-awesome/css/font-awesome.min.css'; 
 
 function Column(props: any) {
   return (
@@ -9,6 +10,7 @@ function Column(props: any) {
       onClick={() => props.handleClick(props.index)}
     >
       {props.title || 'Total'}
+      {props.sorted === "asc" ? <i className="fa fa-sort-up"></i> : props.sorted === "desc" ? <i className="fa fa-sort-down"></i> : null}      
     </TH>
   );
 }

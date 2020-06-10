e5ab5581268dc108284978f72abd5470f4b89380
5maciek <smaciek1@op.pl>
2020-06-10 14:51:20 +0200

    add Column interface

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { TH } from './styled/TH';
 import 'font-awesome/css/font-awesome.min.css'; 
 
-function Column(props: any) {
+interface Column {
+  width?: number;
+  widthColumn?: number;    
+  lastColumn?: boolean;
+  index?: number;
+  title?: string;
+  sorted: string | null;
+  handleClick: (index: number | undefined) => void;  
+};
+
+function Column(props: Column) {
   return (
     <TH
       widthColumn={props.width}
-      lastColumn={props.lastColumn}
+      lastColumn ={props.lastColumn}
       onClick={() => props.handleClick(props.index)}
     >
       {props.title || 'Total'}

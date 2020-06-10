82ee3fdd01c1a400c8bb8307659702ef0763642c
5maciek <smaciek1@op.pl>
2020-06-10 21:00:08 +0200

    small formatting style of component Row

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -5,26 +5,29 @@ import { v4 as uuidv4 } from 'uuid';
 
 interface Row {
   countColumns: number;
-  row: Array<number | string | null>
-};
+  row: Array<number | string | null>;
+}
 
 function Row(props: Row) {
   const sumRow = () => {
-    const sumtotal = props.row.reduce((sum: number, val: any, index: number) => {
-      if (!isNaN(val) && index < props.countColumns) {
-        return sum + val;
-      }
-      return sum;
-    }, 0);
+    const sumtotal = props.row.reduce(
+      (sum: number, val: any, index: number) => {
+        if (!isNaN(val) && index < props.countColumns) {
+          return sum + val;
+        }
+        return sum;
+      },
+      0
+    );
     return sumtotal.toFixed(2);
   };
 
-  const fillEmpty = () => {    
-    if (props.row.length < props.countColumns) {      
+  const fillEmpty = () => {
+    if (props.row.length < props.countColumns) {
       const fillArray: Array<null> = [];
-      for (let i = 0; i < (props.countColumns - props.row.length); i++) {
+      for (let i = 0; i < props.countColumns - props.row.length; i++) {
         fillArray.push(null);
-      }      
+      }
       return fillArray;
     }
   };
@@ -34,10 +37,10 @@ function Row(props: Row) {
       console.log(
         'Some data rows are not displayed because is mismatch with columns.'
       );
-    }    
+    }
   });
 
-  const arr : Array<null> = fillEmpty()!;  
+  const arr: Array<null> = fillEmpty()!;
 
   return (
     <TR>
@@ -47,8 +50,14 @@ function Row(props: Row) {
             {row}
           </TD>
         ) : null
-      )}    
-      {arr ? arr.map((item:null) => <TD key={uuidv4()} lastColumn={false}>{item}</TD>) : null}
+      )}
+      {arr
+        ? arr.map((item: null) => (
+            <TD key={uuidv4()} lastColumn={false}>
+              {item}
+            </TD>
+          ))
+        : null}
       <TD key={uuidv4()} lastColumn={true}>
         {sumRow()}
       </TD>

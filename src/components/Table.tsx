439bfc7394be6d106460c52866ee20e8dbb929ab
5maciek <smaciek1@op.pl>
2020-06-10 10:56:27 +0200

    add new static data

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -4,24 +4,26 @@ import Row from './Row';
 import { StyledTable } from './styled/StyledTable';
 import { TR } from './styled/TR';
 import { v4 as uuidv4 } from 'uuid';
+import 'font-awesome/css/font-awesome.min.css';
 
 class Table extends React.Component {
   state = {
     columns: [
-      { id: uuidv4(), title: 'Header content 1', width: 350 },
-      { id: uuidv4(), title: 'Header content 2', width: 200 },
-      { id: uuidv4(), title: 'Header content 3', width: 200 },
-      { id: uuidv4(), title: 'Header content 4', width: 300 },
-      { id: uuidv4(), title: 'Header content 5', width: 400 },
-      { id: uuidv4(), title: 'Header content 6', width: 200 },
+      { id: uuidv4(), title: 'Header content 1', width: 350, sorted: null },
+      { id: uuidv4(), title: 'Header content 2', width: 200, sorted: null },
+      { id: uuidv4(), title: 'Header content 3', width: 200, sorted: null },
+      { id: uuidv4(), title: 'Header content 4', width: 300, sorted: null },
+      { id: uuidv4(), title: 'Header content 5', width: 400, sorted: null },
+      { id: uuidv4(), title: 'Header content 5', width: 300, sorted: null },
+      { id: uuidv4(), title: 'Header content 6', width: 200, sorted: null },
     ],
     data: [
-      { id: uuidv4(), row: [`Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aspernatur mollitia magnam inventore. Pariatur, optio distinctio. Aliquam architecto dolore voluptate nostrum quis dolores! Voluptatum ipsam illo repellat est quaerat iure.`, 2, 1, 98.02, 'nohow', 'mno'] },
-      { id: uuidv4(), row: ['value 3', 1, 33, 11.12, 'something', 'abc'] },
-      { id: uuidv4(), row: ['value 2', 3, 2, 34.87, 'anything', 'def'] },
-      { id: uuidv4(), row: ['value 1', 3, 2, 71.12, 'everything', 'xyz'] },
-      { id: uuidv4(), row: ['value 0', 3, 2, 1.18, 'somehow', 'pol'] },
-      { id: uuidv4(), row: ['value 5', 3, 2, 147.54, 'nothing', 'trc'] },
+      { id: uuidv4(), row: [`Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aspernatur mollitia magnam inventore. Pariatur, optio distinctio. Aliquam architecto dolore voluptate nostrum quis dolores! Voluptatum ipsam illo repellat est quaerat iure.`, 2, 1, 98.02, 'nohow', 'mno', Math.floor(Math.random()*100)] },
+      { id: uuidv4(), row: ['value 3', 1, 33, 11.12, 'something', 'abc', Math.floor(Math.random()*100)] },
+      { id: uuidv4(), row: ['value 2', 3, 74, 34.87, 'anything', 'def', Math.floor(Math.random()*100)] },
+      { id: uuidv4(), row: ['value 1', 9, 2, 71.12, 'everything', 'xyz', Math.floor(Math.random()*100)] },
+      { id: uuidv4(), row: ['value 0', 5, 1, 1.18, 'somehow', 'pol', Math.floor(Math.random()*100)] },
+      { id: uuidv4(), row: ['value 5', 3, 29, 147.54, 'nothing', 'trc', Math.floor(Math.random()*100)] },
     ],
   };
 

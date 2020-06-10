67b4cbb5e539ffa010baf4aba605a9a35f7891e8
5maciek <smaciek1@op.pl>
2020-06-10 21:01:03 +0200

    typescript refactor of component Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -7,9 +7,9 @@ import { v4 as uuidv4 } from 'uuid';
 import 'font-awesome/css/font-awesome.min.css';
 
 interface State {
-   columns: ColumnData[];
-   data: RowData[];
-};
+  columns: ColumnData[];
+  data: RowData[];
+}
 
 interface ColumnData {
   id: string;
@@ -23,9 +23,8 @@ interface RowData {
   row: Array<number | string | null>;
 }
 
-
-class Table extends React.Component <{}, State>{
-  state : State = {
+class Table extends React.Component<{}, State> {
+  state: State = {
     columns: [
       { id: uuidv4(), title: 'Header content 1', width: 350, sorted: null },
       { id: uuidv4(), title: 'Header content 2', width: 200, sorted: null },
@@ -36,12 +35,78 @@ class Table extends React.Component <{}, State>{
       { id: uuidv4(), title: 'Header content 6', width: 200, sorted: null },
     ],
     data: [
-      { id: uuidv4(), row: [`Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aspernatur mollitia magnam inventore. Pariatur, optio distinctio. Aliquam architecto dolore voluptate nostrum quis dolores! Voluptatum ipsam illo repellat est quaerat iure.`, 2, 1, 98.02, 'nohow', 'mno', Math.floor(Math.random()*100)] },
-      { id: uuidv4(), row: ['value 3', 1, 33, 11.12, 'something', 'abc', Math.floor(Math.random()*100)] },
-      { id: uuidv4(), row: ['value 2', 3, 74, 34.87, 'anything', 'def', Math.floor(Math.random()*100)] },
-      { id: uuidv4(), row: ['value 1', 9, 2, 71.12, 'everything', 'xyz', Math.floor(Math.random()*100)] },
-      { id: uuidv4(), row: ['value 0', 5, 1, 1.18, 'somehow', 'pol', Math.floor(Math.random()*100)] },
-      { id: uuidv4(), row: ['value 5', 3, 29, 147.54, 'nothing', 'trc', Math.floor(Math.random()*100)] },
+      {
+        id: uuidv4(),
+        row: [
+          `Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aspernatur mollitia magnam inventore. Pariatur, optio distinctio. Aliquam architecto dolore voluptate nostrum quis dolores! Voluptatum ipsam illo repellat est quaerat iure.`,
+          2,
+          1,
+          98.02,
+          'nohow',
+          'mno',
+          Math.floor(Math.random() * 100),
+        ],
+      },
+      {
+        id: uuidv4(),
+        row: [
+          'value 3',
+          1,
+          33,
+          11.12,
+          'something',
+          'abc',
+          Math.floor(Math.random() * 100),
+        ],
+      },
+      {
+        id: uuidv4(),
+        row: [
+          'value 2',
+          3,
+          74,
+          34.87,
+          'anything',
+          'def',
+          Math.floor(Math.random() * 100),
+        ],
+      },
+      {
+        id: uuidv4(),
+        row: [
+          'value 1',
+          9,
+          2,
+          71.12,
+          'everything',
+          'xyz',
+          Math.floor(Math.random() * 100),
+        ],
+      },
+      {
+        id: uuidv4(),
+        row: [
+          'value 0',
+          5,
+          1,
+          1.18,
+          'somehow',
+          'pol',
+          Math.floor(Math.random() * 100),
+        ],
+      },
+      {
+        id: uuidv4(),
+        row: [
+          'value 5',
+          3,
+          29,
+          147.54,
+          'nothing',
+          'trc',
+          Math.floor(Math.random() * 100),
+        ],
+      },
     ],
   };
 
@@ -50,10 +115,10 @@ class Table extends React.Component <{}, State>{
     const columns = this.state.columns;
     if (index !== undefined) {
       let sort = columns[index].sorted;
-      let sortedIcons: any = [];
-      let sortedRows: any = [];
+      let sortedIcons: Array<string | null> = [];
+      let sortedRows: RowData[] = [];
       if (sort === null || sort === 'desc') {
-        sortedRows = rows.sort(function (a : any, b : any) {
+        sortedRows = rows.sort(function (a: any, b: any) {
           if (a.row[index] < b.row[index]) {
             return -1;
           }
@@ -63,13 +128,12 @@ class Table extends React.Component <{}, State>{
           return 0;
         });
 
-        sortedIcons = columns.map((column: any, i: number) => {
+        sortedIcons = columns.map((column: ColumnData, i: number) => {
           if (i === index) return 'asc';
           else return null;
-        })
-      }
-      else {
-        sortedRows = rows.sort(function (a: any, b:any) {
+        });
+      } else {
+        sortedRows = rows.sort(function (a: any, b: any) {
           if (a.row[index] < b.row[index]) {
             return 1;
           }
@@ -79,20 +143,22 @@ class Table extends React.Component <{}, State>{
           return 0;
         });
 
-        sortedIcons = columns.map((column: any, i: number) => {
+        sortedIcons = columns.map((column: ColumnData, i: number) => {
           if (i === index) return 'desc';
           else return null;
-        })
+        });
       }
 
-      const newColumns = this.state.columns.map((column: any, i:any) => {
-        return {
-          id: column.id,
-          title: column.title,
-          width: column.width,
-          sorted: sortedIcons[i]
+      const newColumns = this.state.columns.map(
+        (column: ColumnData, i: number) => {
+          return {
+            id: column.id,
+            title: column.title,
+            width: column.width,
+            sorted: sortedIcons[i],
+          };
         }
-      })
+      );
 
       this.setState({
         columns: newColumns,
@@ -108,26 +174,26 @@ class Table extends React.Component <{}, State>{
       <StyledTable>
         <thead>
           <TR>
-            {columns.map((column : any, index : number) => (
+            {columns.map((column: ColumnData, index: number) => (
               <Column
                 key={column.id}
                 index={index}
-                title={column.title}                
+                title={column.title}
                 width={column.width}
                 handleClick={this.handleSortColumn}
-                sorted={column.sorted}                
+                sorted={column.sorted}
               />
             ))}
             <Column
-              key={uuidv4()}              
-              lastColumn={true}              
+              key={uuidv4()}
+              lastColumn={true}
               handleClick={this.handleSortColumn}
               sorted={null}
             />
           </TR>
         </thead>
         <tbody>
-          {rows.map((row : any) => (
+          {rows.map((row: RowData) => (
             <Row
               key={row.id}
               row={row.row}

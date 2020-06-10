cc1a4b38f2a46824824d2b8a2fc52cf41a93b987
5maciek <smaciek1@op.pl>
2020-06-10 20:59:36 +0200

    small formatting refactor of component Column

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { TH } from './styled/TH';
-import 'font-awesome/css/font-awesome.min.css'; 
+import 'font-awesome/css/font-awesome.min.css';
 
 interface Column {
   width?: number;
-  widthColumn?: number;    
+  widthColumn?: number;
   lastColumn?: boolean;
   index?: number;
   title?: string;
   sorted: string | null;
-  handleClick: (index: number | undefined) => void;  
-};
+  handleClick: (index: number | undefined) => void;
+}
 
 function Column(props: Column) {
   return (
     <TH
       widthColumn={props.width}
-      lastColumn ={props.lastColumn}
+      lastColumn={props.lastColumn}
       onClick={() => props.handleClick(props.index)}
     >
       {props.title || 'Total'}
-      {props.sorted === "asc" ? <i className="fa fa-sort-up"></i> : props.sorted === "desc" ? <i className="fa fa-sort-down"></i> : null}      
+      {props.sorted === 'asc' ? (
+        <i className="fa fa-sort-up"></i>
+      ) : props.sorted === 'desc' ? (
+        <i className="fa fa-sort-down"></i>
+      ) : null}
     </TH>
   );
 }
